32c23fcfc8a5f3bf7d8b46889a0c2d27103cc6c1
Nupur Agarwal <nupur.a@accolite.com>
2022-12-07 22:05:38 +0530

    final reactive form

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,4 +1,6 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-active-vendor',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./active-vendor.component.scss']
 })
 export class ActiveVendorComponent implements OnInit {
+htmlForm=new FormGroup({
+name: new FormControl('',Validators.required), 
+email: new FormControl('', Validators.required),
+bio: new FormControl(),
+domain: new FormControl()
+});
 
+
+objectSpace:any;
+
+
+onSubmit(event: any){
+  this.objectSpace =document.getElementById("Object");
+  this.objectSpace.innerHTML= JSON.stringify(this.htmlForm.value) ;
+  // console.log("hello");
+  console.log(this.objectSpace);
+ 
+  // console.log(event.submitter)
+
+}
+
+
+button=document.getElementsByClassName("btn");
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
